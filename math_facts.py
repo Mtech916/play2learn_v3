0c0bea756b971c294ba2f20c947ff106c7b28610
Mtech916 <shredtechfit@gmail.com>
2023-09-10 18:18:55 -0700

    Working math facts program with a few minor issues

--- math_facts.py
+++ math_facts.py
@@ -1,33 +1,139 @@
+import threading
+import random
+import time
+
+game_time = 30
+game_over = threading.Event()
+score = 0
+
+
 def choose_operation():
-    pass
+    """player chooses operation"""
+    operation = None
+    operators = ["+", "-", "*", "/"]
+    while operation not in operators:
+        operation = input(f"Please enter an operation {operators}: ")
+        if operation not in operators:
+            print(f"You entered operator: {operation}")
+            print("That is not a valid operation.")
+            operation = input(f"--Try again-- Please enter an operation {operators}: ")
+    return operation
 
 
 def choose_max_number():
-    pass
+    """player chooses max number"""
+    max_number = None
+    max_num_range = range(1, 100)
+    while max_number not in max_num_range:
+        max_number = int(input("Please enter a max number between 1 and 100: "))
+        if max_number not in max_num_range:
+            print(f"You entered max number: {max_number}")
+            print("That is not a valid number.")
+            max_number = int(
+                input("--Try again-- Please enter a max number between 1 and 100: ")
+            )
+    return max_number
 
 
 def start_timer():
-    pass
+    """Start the timer."""
+    global game_time, game_over
+    while game_time > 0 and not game_over.is_set():
+        time.sleep(1)
+        game_time -= 1
+    # if game_time == 0:
+    #     print("\nTime's up!")
+    #     end_game()
 
 
-def generate_problem():
-    pass
+def generate_problem(operation, max_number):
+    """Generate a random math problem."""
+    rand_num1 = random.randint(1, max_number)
+    rand_num2 = random.randint(1, max_number)
+
+    if operation == "/":
+        while rand_num1 % rand_num2 != 0:
+            rand_num1 = random.randint(1, max_number)
+            # rand_num2 = random.randint(1, max_number)
+
+    math_problem = f"{rand_num1} {operation} {rand_num2} = ?"
+
+    return math_problem, rand_num1, rand_num2
 
 
-def check_answer():
-    pass
+def check_answer(operation, rand_num1, rand_num2, user_answer):
+    """Check if user answer is truthy"""
+
+    if operation == "+":
+        answer = rand_num1 + rand_num2
+    elif operation == "-":
+        answer = rand_num1 - rand_num2
+    elif operation == "*":
+        answer = rand_num1 * rand_num2
+    elif operation == "/":
+        answer = rand_num1 / rand_num2
+
+    if user_answer == answer:
+        return True
+    else:
+        return False
 
 
 def end_game():
-    pass
+    """End the game."""
+    global score
+
+    game_over.set()
+
+    print(f"\nSorry, you didn't get that answer in on time.")
+    print(f"\nYour score is {score}.")
+
+    time.sleep(1)
+    reset_game()
 
 
-def play_again():
-    pass
+def reset_game():
+    global game_time, score, game_over
+
+    play_again = input("Press enter to play again or 'q' to quit: ")
+    if play_again.upper() == "Q":
+        return
+    else:
+        game_time = 30
+        score = 0
+        game_over = threading.Event()
+        # game_over.clear()
+
+    time.sleep(1)
+    main()
 
 
 def main():
-    pass
+    global game_time, score, game_over
+    operation = choose_operation()
+    max_number = choose_max_number()
+
+    if operation and max_number:
+        timer_thread = threading.Thread(target=start_timer)
+        timer_thread.start()
+
+    while not game_over.is_set():
+        display_problem, rand_num1, rand_num2 = generate_problem(operation, max_number)
+        print(f"{display_problem}")
+        print(f"You have {game_time} seconds left.")
+        user_answer = int(input("Enter an answer: "))
+        checked = check_answer(operation, rand_num1, rand_num2, user_answer)
+
+        if checked and game_time > 0:
+            score += 1
+            print(f"{user_answer} is correct!")
+            print("-" * 50)
+        else:
+            if game_time == 0:
+                # game_over.set()
+                end_game()
+            else:
+                print(f"{user_answer} is not correct. Please try again.")
 
 
 if __name__ == "__main__":
--- practice_math_facts.py
+++ practice_math_facts.py
@@ -1,33 +1,163 @@
+import threading
+import random
+import time
+
+timer_lock = threading.Lock()
+game_time = 30
+timer_stop = threading.Event()
+score = 0
+
+
 def choose_operation():
-    pass
+    """player chooses operation"""
+    operation = None
+    operators = ["+", "-", "*", "/"]
+    while operation not in operators:
+        operation = input(f"Please enter an operation {operators}: ")
+        if operation not in operators:
+            print(f"You entered operator: {operation}")
+            print("That is not a valid operation.")
+            operation = input(f"--Try again-- Please enter an operation {operators}: ")
+    return operation
 
 
 def choose_max_number():
-    pass
+    """player chooses max number"""
+    max_number = None
+    max_num_range = range(1, 100)
+    while max_number not in max_num_range:
+        max_number = int(input("Please enter a max number between 1 and 100: "))
+        if max_number not in max_num_range:
+            print(f"You entered max number: {max_number}")
+            print("That is not a valid number.")
+            max_number = int(
+                input("--Try again-- Please enter a max number between 1 and 100: ")
+            )
+    return max_number
 
 
 def start_timer():
-    pass
+    """Start the timer."""
+    global game_time, 
+    while game_time > 0 and not timer_stop.is_set():
+        time.sleep(1)
+        with timer_lock:
+            game_time -= 1
 
 
-def generate_problem():
-    pass
+def generate_problem(operation, max_number):
+    """Generate a random math problem."""
+    rand_num1 = random.randint(1, max_number)
+    rand_num2 = random.randint(1, max_number)
+
+    if operation == "/":
+        while rand_num1 % rand_num2 != 0:
+            rand_num1 = random.randint(1, max_number)
+            # rand_num2 = random.randint(1, max_number)
+
+    math_problem = f"{rand_num1} {operation} {rand_num2} = ?"
+
+    return math_problem, rand_num1, rand_num2
 
 
-def check_answer():
-    pass
+def check_answer(operation, rand_num1, rand_num2, user_answer):
+    """Check if user answer is truthy"""
+
+    if operation == "+":
+        answer = rand_num1 + rand_num2
+    elif operation == "-":
+        answer = rand_num1 - rand_num2
+    elif operation == "*":
+        answer = rand_num1 * rand_num2
+    elif operation == "/":
+        answer = rand_num1 / rand_num2
+
+    if user_answer == answer:
+        return True
+    else:
+        return False
 
 
 def end_game():
-    pass
+    """End the game."""
+    global score, game_time, timer_stop
+
+    print("\nTime's up!")
+    print(f"\nSorry, you didn't get that answer in on time.")
+    print(f"\nYour score is {score}.")
+
+    play_again = input(f"Press enter to play again or 'q' to quit: ")
+
+    if not play_again:
+        timer_stop.set()
+        reset_game()
+    elif play_again.lower() == "q":
+        timer_stop.set()
+        return
+    else:
+        timer_stop.set()
+        return
 
 
-def play_again():
-    pass
+def reset_game():
+    """Reset the game."""
+    global game_time, score, timer_lock, timer_stop
+
+    score = 0
+    timer_lock = threading.Lock()
+    game_time = 30
+    timer_stop = threading.Event()
+
+    time.sleep(1)
+
+    main()
 
 
 def main():
-    pass
+    """Run the game."""
+    global game_time
+    global score
+
+    operation = choose_operation()
+
+    max_number = choose_max_number()
+
+    # print(f"From main -> operation: {operation}")
+    # print(f"From main -> max_number: {max_number}")
+
+    if operation and max_number:
+        timer_thread = threading.Thread(target=start_timer)
+        timer_thread.start()
+
+    while game_time > 0:
+        display_problem, rand_num1, rand_num2 = generate_problem(operation, max_number)
+
+        # print(f"From main -> checked: {checked}")
+        # print(f"From main -> user_answer: {user_answer}")
+
+        correct_answer = False
+
+        while not correct_answer:
+            print(f"{display_problem}")
+            print(f"You have {game_time} seconds left.")
+            user_answer = int(input("Enter an answer: "))
+            checked = check_answer(operation, rand_num1, rand_num2, user_answer)
+
+            if not checked:
+                if game_time == 0:
+                    # print(f"\nSorry, you didn't get that answer in on time.")
+                    end_game()
+                else:
+                    print(f"{user_answer} is not correct. Please try again.")
+            else:
+                if game_time == 0:
+                    # print(f"\nSorry, you didn't get that answer in on time.")
+                    end_game()
+                else:
+                    score += 1
+                    print(f"{user_answer} is correct!")
+                    print("-" * 50)
+                    correct_answer = True
 
 
 if __name__ == "__main__":
